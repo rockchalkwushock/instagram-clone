9d5c6823194bfd2f7d054d7b3639519571d81ba6
Cody Brunner <rockchalkwushock@icloud.com>
2018-02-17 08:12:23 -0600

    adds two views and navigation.

---
+++ mobile/src/screens/ExploreScreen/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+class ExploreScreen extends Component {
+  state = {}
+  render() {
+    return (
+      <View>
+        <Text>ExploreScreen</Text>
+      </View>
+    )
+  }
+}
+
+export default ExploreScreen
---
+++ mobile/src/screens/FeedsScreen/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+class FeedsScreen extends Component {
+  state = {}
+  render() {
+    return (
+      <View>
+        <Text>FeedsScreen</Text>
+      </View>
+    )
+  }
+}
+
+export default FeedsScreen
